d8abbb6eef4689af11fab4a259985ba4628129b1
Jean-Philippe Choquette <jpchoquette@lichencommunications.com>
2020-04-22 15:47:09 -0400

    debut style typer lichen

--- typer.js
+++ typer.js
@@ -2,17 +2,18 @@
 let score = 0;
 let lives = 10;
 let caseSensitive = true;
-
+let pause = false;
 const center = {
   x: canvas.width / 2,
   y: canvas.height / 2,
   radius: 20,
-  color: '#FF0000'
+  color: '#2196F3',
+  border: 'solid 10px red'
 };
 
 const letter = {
-  font: '20px Arial',
-  color: '#0095DD',
+  font: '40px Red Hat Text',
+  color: '#090121',
   size: 30,
   highestSpeed: 1.6,
   lowestSpeed: 0.6,
@@ -24,24 +25,31 @@ let letters = [];
 document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
 window.addEventListener('resize', resizeHandler);
+document.getElementById('change-case-text').innerHTML = 'Sensible';
+document.getElementById('livesText').innerHTML = lives + ((lives > 1 || lives === 0) ? ' vies' : ' vie');
+document.getElementById('scoreText').innerHTML = score;
 
 loop(function (frames) {
-  paintCircle(center.x, center.y, center.radius, center.color);
-  ctx.font = letter.font;
-  ctx.fillStyle = letter.color;
-  for (const l of letters) {
-    ctx.fillText(String.fromCharCode(l.code), l.x, l.y);
+  if (pause) {
+  } else {
+    paintCircle(center.x, center.y, center.radius, center.color);
+    // console.log('ctx', ctx)
+    ctx.font = letter.font;
+    ctx.fillStyle = letter.color;
+    for (const l of letters) {
+      ctx.fillText(String.fromCharCode(l.code), l.x, l.y);
+    }
+    paintParticles();
+    ctx.font = '30px Paytone One';
+    ctx.fillStyle = '#090121';
+    ctx.margin = '30px'
+    ctx.fillText('Pointage: ' + score, label.left, label.margin);
+    ctx.fillText('Vies: ' + lives, label.right, label.margin);
+    processParticles(frames);
+    createLetters();
+    removeLetters(frames);
   }
-  paintParticles();
-  ctx.font = label.font;
-  ctx.fillStyle = label.color;
-  ctx.fillText('Score: ' + score, label.left, label.margin);
-  ctx.fillText('Lives: ' + lives, label.right, label.margin);
-  processParticles(frames);
-  createLetters();
-  removeLetters(frames);
 });
-
 function createLetters () {
   if (Math.random() < letter.probability) {
     const x = Math.random() < 0.5 ? 0 : canvas.width;
@@ -63,19 +71,37 @@ function createLetters () {
 function removeLetters (frames) {
   for (const l of letters) {
     if (isIntersectingRectangleWithRectangle(l, letter.size, letter.size, center, center.radius, center.radius)) {
+      pause = true
       if (--lives === 0) {
-        window.alert('GAME OVER!');
-        window.location.reload(false);
+        document.getElementById('modal-button').click();
+        // window.alert('Partie terminée');
+        // window.location.reload(false);
       } else if (lives > 0) {
-        window.alert('START AGAIN!');
+        document.getElementById('modal-button').click();
+        // window.alert('Continuer');
         letters = [];
       }
-      break;
+      document.getElementById('livesText').innerHTML = lives + ((lives > 1 || lives === 0) ? ' vies' : ' vie');
+      document.getElementById('scoreText').innerHTML = score;
+
     } else {
       l.x += l.speedX * frames;
       l.y += l.speedY * frames;
     }
   }
+}
+function continueGame () {
+  document.getElementById('modal-button-close').click();
+  // if (--lives === 0) {
+  //   // window.alert('Partie terminée');
+  //   document.getElementById('modal-button-close').click();
+  //   // window.location.reload(false);
+  // } else if (lives > 0) {
+  //   // window.alert('Continuer');
+  //   document.getElementById('modal-button-close').click();
+  //   letters = [];
+  // }
+  pause = false
 }
 
 function type (i, l) {
@@ -87,9 +113,9 @@ function type (i, l) {
 window.changeCase = function () {
   caseSensitive = !caseSensitive;
   if (caseSensitive) {
-    document.getElementById('change-case-text').innerHTML = '';
+    document.getElementById('change-case-text').innerHTML = 'Sensible';
   } else {
-    document.getElementById('change-case-text').innerHTML = 'in';
+    document.getElementById('change-case-text').innerHTML = 'Non-sensible';
   }
 };
 
@@ -116,7 +142,12 @@ function keyDownHandler (e) {
         }
       }
     }
+    // let classesToAdd = [ 'animated', 'shake', 'faster' ];
+    document.getElementById('canvas').classList.add('shake');
     score--;
+    setTimeout(() => {
+      document.getElementById('canvas').classList.remove('shake');
+    }, 1000)
   }
 }
 
